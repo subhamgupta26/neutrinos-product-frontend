deb4a6befa422b889313d6e2881e953bd60b3fde
Subham Gupta <subham_gupta@persistent.co.in>
2018-07-09 12:27:13 +0530

    skeleton components and services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/home/login/login.component';
+import { SignupComponent } from './components/home/signup/signup.component';
+import { ProductsHomeComponent } from './components/user/products-home/products-home.component';
+import { CartComponent } from './components/user/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ProductsHomeComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule
