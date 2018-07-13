9decde779560f7b4fc50cfa8232dd7005ca558ce
Subham Gupta <subham_gupta@persistent.co.in>
2018-07-13 11:49:28 +0530

    css updated

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  token = localStorage.getItem('token');
+  userName= '';
+  isToken = false;
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.setUserName();
   }
 
+
+
+  setUserName() {
+    if ( this.token ) {
+      this.isToken = true;
+      this.userService.getCurrentUser().subscribe((response) => {
+        this.userName = response['name'];
+      });
+    }
+  }
 }
